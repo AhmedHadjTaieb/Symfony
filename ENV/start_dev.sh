cc9592af60e54efccf32f25ce76809291b3b4215
Ahmed  HADJ TAIEB <ahmed.hadjtaieb@debian>
2018-12-25 11:56:04 +0100

    change perfermance docker

---
+++ ENV/start_dev.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo chown -R $(whoami):$(whoami) $(pwd)
+setfacl -dR -m u:$(whoami):rwX -m u:$(whoami):rwX $(pwd)
+setfacl -R -m u:$(whoami):rwX -m u:$(whoami):rwX $(pwd)
+sudo chown -R www-data:www-data $(pwd)
+
+docker-compose --project-name opclroom -f $(pwd)/ENV/docker-compose.yml  down -v
+docker-compose --project-name opclroom -f $(pwd)/ENV/docker-compose.yml up --build -d
+
