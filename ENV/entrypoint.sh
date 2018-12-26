04b1466132a5863c23d3947816afb10e303ef001
AhmedHadjTaieb <ahmed.ht@gmx.com>
2018-12-26 17:07:53 +0100

    fixed docker for macos

--- ENV/entrypoint.sh
+++ ENV/entrypoint.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 cd /var/www/html
 wget http://cs.sensiolabs.org/download/php-cs-fixer-v2.phar -O php-cs-fixer
-set -e
-# vars
-host="$1"
-port="$2"
 waitMoment=0
 # main function
   >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ## Wait for Mysql";
-while ! nc -z "$host" "$port"; do
+while ! nc -z "$MYSQL_HOST" "$MYSQL_PORT"; do
       sleep 5;
       let "waitMoment += 5"
     >&2 echo " $(date '+%H:%M:%S')  --> Waiting for Mysql $waitMoment seconds";
 done
-   >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ## $host is up - executing command after  $waitMoment  seconds"
+   >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ## $MYSQL_HOST is up - executing command after  $waitMoment  seconds"
   >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ## Install despondencys"
   composer config --global process-timeout 2000
 php -d memory_limit=-1 /usr/local/bin/composer install -o -vvv
@@ -29,4 +25,5 @@ chmod -R 777 web/uploads
 # app/console fos:elastica:populate
 #  >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ## ca:cl"
   >&2 echo " $(date '+%Y-%m-%d %H:%M:%S')  ##  Run apache in foreground"
+
 apachectl -D FOREGROUND
