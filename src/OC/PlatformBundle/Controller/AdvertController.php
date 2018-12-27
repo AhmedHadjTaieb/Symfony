219b8be7fd6dedc9146213bf5ab379b9aff04e77
AhmedHadjTaieb <ahmed.ht@gmx.com>
2018-12-27 21:21:29 +0100

    add global varriable in twig

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,46 +9,52 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
 
-class AdvertController extends Controller {
+class AdvertController extends Controller
+{
 
-  public function indexAction() {
+    public function indexAction()
+    {
 
-    return $this->render('@OCPlatform/Advert/hello.html.twig', array('nom' => 'AhmedHt'));
-  }
+        return $this->render('@OCPlatform/Advert/hello.html.twig', array('nom' => 'AhmedHt'));
+    }
 
-  public function viewAction($id, Request $request) {
-    $tag = $request->query->get('tag');
-    $session = $request->getSession();
-    $session->set('user_id', 91);
-    $userId = $session->get('user_id');
+    public function viewAction($id, Request $request)
+    {
+        $tag = $request->query->get('tag');
+        $session = $request->getSession();
+        $session->set('user_id', 91);
+        $userId = $session->get('user_id');
+        $liste = array('a' => 'A', 'b' => 'B', 'c' => 'C');
 
+        return $this->render('@OCPlatform/Advert/view.html.twig', array(
+            'id' => $id,
+            'tag' => $tag,
+            'user_id' => $userId,
+            'liste' => $liste
+        ));
+    }
 
-    return $this->render('@OCPlatform/Advert/view.html.twig', array(
-      'id' => $id,
-      'tag' => $tag,
-      'user_id' => $userId
-    ));
-  }
+    public function addAction(Request $request)
+    {
+        $session = $request->getSession();
 
-  public function addAction(Request $request) {
-    $session = $request->getSession();
+        $session->getFlashBag()->add('info', 'Bonjour Mr');
+        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
 
-    $session->getFlashBag()->add('info', 'Bonjour Mr');
-    return $this->redirectToRoute('oc_platform_view', array('id' => 5));
-  }
+    public function editAction()
+    {
 
-  public function editAction() {
+        return $this->render('@OCPlatform/Advert/bye.html.twig', array('nom' => 'AhmedHt'));
+    }
 
-    return $this->render('@OCPlatform/Advert/bye.html.twig', array('nom' => 'AhmedHt'));
-  }
+    public function deleteAction()
+    {
 
-  public function deleteAction() {
-
-    return $this->render('@OCPlatform/Advert/bye.html.twig', array('nom' => 'AhmedHt'));
-  }
+        return $this->render('@OCPlatform/Advert/bye.html.twig', array('nom' => 'AhmedHt'));
+    }
 
 }
